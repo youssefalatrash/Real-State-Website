fe9dd1a5779e79ced30607508314532a41522fbc
Youssef Alatrash <104606746+youssefalatrash@users.noreply.github.com>
2023-10-30 10:33:38 +0200

    Add files via upload

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Companies.css";
+const Companies = () => {
+  return (
+    <section className="c-rapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" />
+        <img src="./tower.png" />
+        <img src="./equinix.png" />
+        <img src="./realty.png" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
